26532d1df3409958ba901184afd8d3098a3ee9a7
Iqbalaulia <miqbalar12@gmail.com>
2020-09-27 00:42:28 +0700

    initial commit

---
+++ src/components/Quote.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <div id="quote">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["quote"],
+  name: "quote",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+div {
+  border: 1px solid #cccccc;
+  box-shadow: 1px 1px 2px black;
+  padding: 30px;
+  margin: 30px auto;
+  text-align: center;
+}
+</style>
